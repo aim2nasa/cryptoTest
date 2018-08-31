708d9ad982200967408cebab9dde43bd56fe03ca
Rossi Kwak <rossi.kwak@gmail.com>
2018-08-31 13:17:53 +0900

    add 1st crypto++ library testing code, which does not requite library link

--- crypto++/gtest/aesBase.cpp
+++ crypto++/gtest/aesBase.cpp
@@ -1,7 +1,9 @@
 #include <gtest/gtest.h>
+#include <cryptopp/aes.h>
  
 TEST(AesParameterTest, defaultValues) { 
 	ASSERT_EQ(0, 0);
+	ASSERT_EQ((unsigned int)CryptoPP::AES::DEFAULT_KEYLENGTH, 16);
 }
 
 int main(int argc, char **argv) {
