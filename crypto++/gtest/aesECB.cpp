f79f32ab8a32ff71f685ae077fea05dd6204066c
Rossi Kwak <rossi.kwak@gmail.com>
2018-09-04 14:58:03 +0900

    create AES ECB encryption instance and link with crypto library for the first time

--- crypto++/gtest/aesECB.cpp
+++ crypto++/gtest/aesECB.cpp
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
 #include <cryptopp/aes.h>
+#include <cryptopp/modes.h>
  
 TEST(AesECBTest, encryptDescrypt_Key128bit) { 
 	const unsigned int keySize = CryptoPP::AES::DEFAULT_KEYLENGTH;
@@ -8,4 +9,6 @@ TEST(AesECBTest, encryptDescrypt_Key128bit) {
 	memset(key,0,keySize);
 
 	std::string plainText = "This is test";
+
+	CryptoPP::ECB_Mode<CryptoPP::AES>::Encryption e;
 }
