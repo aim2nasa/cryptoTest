424a714c4902f853a31a36213d5f8dfc8c56c331
Rossi Kwak <rossi.kwak@gmail.com>
2018-09-04 13:11:15 +0900

    move main function to main.cpp and modity Makefile to account for this

--- crypto++/gtest/aesBase.cpp
+++ crypto++/gtest/aesBase.cpp
@@ -6,8 +6,3 @@ TEST(AesParameterTest, defaultValues) {
 	ASSERT_EQ((unsigned int)CryptoPP::AES::DEFAULT_KEYLENGTH, 16);
 	ASSERT_EQ((unsigned int)CryptoPP::AES::BLOCKSIZE, 16);
 }
-
-int main(int argc, char **argv) {
-	testing::InitGoogleTest(&argc, argv);
-	return RUN_ALL_TESTS();
-}
