1b0fe4e2c85386f91c71bbb722e9be23d9daa5b0
Rossi Kwak <rossi.kwak@gmail.com>
2018-09-10 16:24:42 +0900

    create aesCCMTest class and define encrypt test fixture using it

--- crypto++/gtest/aesCCM.cpp
+++ crypto++/gtest/aesCCM.cpp
@@ -7,6 +7,56 @@
 #define TAG_SIZE 8
 #define CCM_MAX_IV_SIZE 13
 
+class aesCCMTest: public ::testing::Test {
+public:
+	std::string enc(std::string aad,std::string plainText);
+
+	virtual void SetUp() {
+		std::cout<<"Setup start"<<std::endl;
+		keySize_ = 32;
+		ivSize_ = CCM_MAX_IV_SIZE;
+
+		key_ = new byte[keySize_];
+		iv_ = new byte[ivSize_];
+		memset(key_,0,keySize_);
+		memset(iv_,0,ivSize_);
+		std::cout<<"Setup end"<<std::endl;
+	}
+	virtual void TearDown() {
+		std::cout<<"Teardown start"<<std::endl;
+		delete [] iv_;
+		delete [] key_;
+		std::cout<<"Teardown stop"<<std::endl;
+	}
+
+	int keySize_,ivSize_;
+	byte *key_,*iv_;
+};
+
+std::string aesCCMTest::enc(std::string aad,std::string plainText){
+	CryptoPP::CCM<CryptoPP::AES,TAG_SIZE>::Encryption e;
+	e.SetKeyWithIV(key_,keySize_,iv_,ivSize_);
+	e.SpecifyDataLengths(aad.size(),plainText.size(),0);
+
+	std::string cipherText;
+	CryptoPP::AuthenticatedEncryptionFilter ef(e,new CryptoPP::StringSink(cipherText) );
+
+	ef.ChannelPut("AAD",(const byte*)aad.data(),aad.size());
+	ef.ChannelMessageEnd("AAD");
+
+	ef.ChannelPut("",(const byte*)plainText.data(),plainText.size());
+	ef.ChannelMessageEnd("");
+	return cipherText;
+}
+
+TEST_F(aesCCMTest,encrypt) {
+	std::string plainText = "AE CCM test";
+	std::string cipherText = enc("AAD",plainText);
+
+	EXPECT_EQ(plainText,"AE CCM test");
+	EXPECT_EQ(toHexStr(cipherText),"9CDB64EB4BB626AC2D4F5A6483C1EC756305C4");
+}
+
 void aesCcmEncDec(unsigned int keySizeInBytes){
 	const unsigned int keySize = keySizeInBytes;
 
