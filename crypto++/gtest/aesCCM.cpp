3b13ca941f4bb9bd3a0ab7fd05e4413d52e439c2
Rossi Kwak <rossi.kwak@gmail.com>
2018-09-11 16:32:33 +0900

    define asEncryption,adDecryption which instantiate cipher operation. Note: Instantiation of template class  must be determined in the compile time, not the run time.

--- crypto++/gtest/aesCCM.cpp
+++ crypto++/gtest/aesCCM.cpp
@@ -12,6 +12,8 @@ public:
 	bool dec(std::string aad,std::string cipherText,std::string& decodedText);
 	void encTest(int keySize,int ivSize,std::string aad,std::string plainText,std::string cipherTextHexStr);
 	void encDecTest(int keySize,int ivSize,std::string aad,std::string plainText);
+	CryptoPP::AuthenticatedSymmetricCipher* asEncryption(int tagSize);
+	CryptoPP::AuthenticatedSymmetricCipher* asDecryption(int tagSize);
 
 	void setUp(int keySize,int ivSize) {
 		keySize_ = keySize;
@@ -96,6 +98,62 @@ void aesCCMTest::encDecTest(int keySize,int ivSize,std::string aad,std::string p
 	EXPECT_EQ(dec(aad,enc(aad,plainText),recoveredText),true);
 	EXPECT_EQ(recoveredText,plainText);
 	tearDown();
+}
+
+CryptoPP::AuthenticatedSymmetricCipher* aesCCMTest::asEncryption(int tagSize){
+	CryptoPP::AuthenticatedSymmetricCipher* p = NULL;
+	switch(tagSize){
+	case 4:
+		p = new CryptoPP::CCM<CryptoPP::AES,4>::Encryption;
+		break;
+	case 6:
+		p = new CryptoPP::CCM<CryptoPP::AES,6>::Encryption;
+		break;
+	case 8:
+		p = new CryptoPP::CCM<CryptoPP::AES,8>::Encryption;
+		break;
+	case 10:
+		p = new CryptoPP::CCM<CryptoPP::AES,10>::Encryption;
+		break;
+	case 12:
+		p = new CryptoPP::CCM<CryptoPP::AES,12>::Encryption;
+		break;
+	case 14:
+		p = new CryptoPP::CCM<CryptoPP::AES,14>::Encryption;
+		break;
+	case 16:
+		p = new CryptoPP::CCM<CryptoPP::AES,16>::Encryption;
+		break;
+	}
+	return p;
+}
+
+CryptoPP::AuthenticatedSymmetricCipher* aesCCMTest::asDecryption(int tagSize){
+	CryptoPP::AuthenticatedSymmetricCipher* p = NULL;
+	switch(tagSize){
+	case 4:
+		p = new CryptoPP::CCM<CryptoPP::AES,4>::Decryption;
+		break;
+	case 6:
+		p = new CryptoPP::CCM<CryptoPP::AES,6>::Decryption;
+		break;
+	case 8:
+		p = new CryptoPP::CCM<CryptoPP::AES,8>::Decryption;
+		break;
+	case 10:
+		p = new CryptoPP::CCM<CryptoPP::AES,10>::Decryption;
+		break;
+	case 12:
+		p = new CryptoPP::CCM<CryptoPP::AES,12>::Decryption;
+		break;
+	case 14:
+		p = new CryptoPP::CCM<CryptoPP::AES,14>::Decryption;
+		break;
+	case 16:
+		p = new CryptoPP::CCM<CryptoPP::AES,16>::Decryption;
+		break;
+	}
+	return p;
 }
 
 TEST_F(aesCCMTest,encrypt) {
